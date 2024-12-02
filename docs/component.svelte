7159dd8c7168b54d424944f426fbff92d9354529
reaper <ahoy@barelyhuman.dev>
2024-12-03 01:48:44 +0530

    refactor: add in pre-rendering for svelte files

--- docs/component.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-    let name = 0
-    setInterval(() => {
-        name += 1
-    }, 1000)
-</script>
-
-<h1>Hello {name}!</h1>
---
+++ docs/svelte-example.svelte
@@ -0,0 +1,13 @@
+<script>
+    let numbers = $state([1, 2, 3, 4])
+    let total = $derived(numbers.reduce((t, n) => t + n, 0))
+    function addNumber() {
+        numbers[numbers.length] = numbers.length + 1
+    }
+</script>
+
+<p>{numbers.join(' + ')} = {total}</p>
+
+<button onclick={addNumber} class="px-4 py-2 text-white bg-black rounded">
+    Add a number
+</button>
