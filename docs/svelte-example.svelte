6f0f66cb500f3eb8db2cf31a4d87e49e53afc319
reaper <ahoy@barelyhuman.dev>
2024-12-08 17:10:41 +0530

    feat: add css processing and scoped styles

--- docs/svelte-example.svelte
+++ docs/svelte-example.svelte
@@ -1,4 +1,5 @@
 <script>
+    import Button from './components/button.svelte'
     let numbers = $state([1, 2, 3, 4])
     let total = $derived(numbers.reduce((t, n) => t + n, 0))
     function addNumber() {
@@ -11,3 +12,5 @@
 <button onclick={addNumber} class="px-4 py-2 text-white bg-black rounded">
     Add a number
 </button>
+
+<Button />
